090f0d0a5437013da5f3073ddc68637243c6f5ea
Arnold <a1121250001@utch.edu.mx>
2022-02-09 13:50:47 -0700

    Dos

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Foco from './src/Components/Foco';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Foco/>
     </View>
   );
 }
@@ -13,7 +12,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#C2FFF6',
     alignItems: 'center',
     justifyContent: 'center',
   },
